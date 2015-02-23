816667a138a5f1b3ab5faa6e7d685a5f6602e121
None None <noneenon333@gmail.com>
2015-02-23 00:46:43 -0500

    Adding downloading of the necessary FW files (for sound, bt, and wifi)

--- run_commands_in_chroot.sh
+++ run_commands_in_chroot.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -x
 
+cpus_count=`cat /proc/cpuinfo | grep processor | wc -l`
+work_dir=/usr/src
+build_dir=${work_dir}/kernel_build
+
 # Setup locale and timezone
 cp /usr/share/zoneinfo/America/Toronto /etc/localtime
 echo 'LANG="en_US.UTF-8"' >  /etc/default/locale
@@ -10,11 +14,8 @@ dpkg-reconfigure -f non-interactive tzdata
 
 # Download the necessary packages to chroot for compiling and signing
 apt-get update -y
-apt-get install -y vim wget make bc vboot-kernel-utils
+apt-get install -y vim wget make bc vboot-kernel-utils git wireless-tools wpasupplicant
 
-cpus_count=`cat /proc/cpuinfo | grep processor | wc -l`
-work_dir=/usr/src
-build_dir=${work_dir}/kernel_build
 cd $work_dir
 mkdir ${build_dir}
 cp .config ${build_dir}
@@ -25,8 +26,19 @@ make O=${build_dir} oldconfig
 make O=${build_dir} -j${cpus_count}
 make O=${build_dir} modules_install firmware_install install headers_install
 
-# sign the newly built kernel
+# Sign the newly built kernel
 vbutil_kernel --pack ${work_dir}/signed_kernel.bin --keyblock /usr/share/vboot/devkeys/kernel.keyblock --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk --version 1 --vmlinuz /boot/vmlinuz-3.19.0 --config ${work_dir}/kernel_cmdline.txt --arch x86
+
+# Download FW for sound and bluetooth
+cd ${work_dir}
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
+cp -avf linux-firmware/intel /lib/firmware/
+
+# Download FW for WiFi
+cd ${work_dir}
+wget https://wireless.wiki.kernel.org/_media/en/users/drivers/iwlwifi-7260-ucode-23.13.10.0.tgz
+tar xvzf iwlwifi-7260-ucode-23.13.10.0.tgz
+cp iwlwifi-7260-ucode-23.13.10.0/iwlwifi-7260-10.ucode /lib/firmware
 
 
 set +x
