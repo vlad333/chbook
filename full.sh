d05f66787e76be4ee8bc207703253d440ecaec9f
None None <noneenon333@gmail.com>
2015-02-23 00:46:03 -0500

    Fixing the minor error

--- full.sh
+++ full.sh
@@ -5,8 +5,8 @@ target=trusty
 chroot_dir=trusty_rootfs
 
 # Create chroot 
-mkdir ${chroot_dir}
-sudo debootstrap --arch=${arch} --variant=buildd ${target} ${chroot_dir}
+#mkdir ${chroot_dir}
+#sudo debootstrap --arch=${arch} --variant=buildd ${target} ${chroot_dir}
 
 # Need to bind /dev, /dev/pts, /proc, and /sys before entering chroot
 sudo mount --bind /dev $chroot_dir/dev
@@ -18,13 +18,14 @@ sudo mount -t sysfs sys $chroot_dir/sys
 apt_sources_file=/etc/apt/sources.list
 chroot_apt_sources_file=${chroot_dir}${apt_sources_file}
 sudo cp ${apt_sources_file} ${chroot_apt_sources_file}
-chroot_work_dir=${chroot_dir}/usr/src
+work_dir=/usr/src
+chroot_work_dir=${chroot_dir}${work_dir}
 sudo cp .config ${chroot_work_dir}
 sudo cp kernel_cmdline.txt ${chroot_work_dir}
 sudo cp run_commands_in_chroot.sh ${chroot_work_dir}
 
 # Run script in chroot
-sudo chroot ${chroot_dir} /bin/bash -x ${chroot_work_dir}/run_commands_in_chroot.sh
+sudo chroot ${chroot_dir} /bin/bash -x ${work_dir}/run_commands_in_chroot.sh
 
 # Need to inbind /dev, /dev/pts, /proc, and /sys after leaving chroot
 sudo umount $chroot_dir/dev/pts
